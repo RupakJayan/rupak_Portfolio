710f91b85107ea527549fb8115aaba70359fd9e4
Rupak jayan B <rupakjayan22@gmail.com>
2024-08-12 00:33:26 +0530

    12/08/2024

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}', // Add the paths to all of your content files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
