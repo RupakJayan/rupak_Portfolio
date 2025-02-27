78306c26146a8658c71406027bab573c7e53f9b0
RupakJayan <rupakjayan22@gmail.com>
2025-02-27 15:43:11 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import React from "react";
 import react from "@vitejs/plugin-react";
+
 import path from "path";
 
 export default defineConfig({
@@ -13,21 +15,21 @@ export default defineConfig({
   server: {
     port: 3000, // Optional: Change dev server port
   },
-  build: {
-    chunkSizeWarningLimit: 3000,
-    outDir: "dist", // Ensure Vercel finds the output
-    emptyOutDir: true, // Clears old builds
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            if (id.includes("three") || id.includes("three-stdlib")) {
-              return "three"; // Separate Three.js
-            }
-            return "vendor"; // Code splitting for better performance
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   chunkSizeWarningLimit: 3000,
+  //   outDir: "dist", // Ensure Vercel finds the output
+  //   emptyOutDir: true, // Clears old builds
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: (id) => {
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("three") || id.includes("three-stdlib")) {
+  //             return "three"; // Separate Three.js
+  //           }
+  //           return "vendor"; // Code splitting for better performance
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
