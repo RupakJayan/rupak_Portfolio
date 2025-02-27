03469d2468620c3e2ba13acb07788f3b9c9f8c2e
RupakJayan <rupakjayan22@gmail.com>
2025-02-27 19:59:44 +0530

    updated code

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Allows cleaner imports
